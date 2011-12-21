fb807774bc9136858dcace187a447e5941ac7596
DetegrMBP <@>
2011-12-21 21:09:12 +0200

    Graphics classes

---
+++ Graphics.cpp
@@ -0,0 +1,18 @@
+#include "Graphics.h"
+
+namespace pwskoag
+{
+	RendererImpl* 	Renderer::impl;
+	uint			Renderer::references;
+
+	Renderer::Renderer(uint w, uint h)
+	{
+		if(!references){impl=new RendererImpl(w,h); ++references;}
+		else {++references;}
+	}
+	Renderer::~Renderer()
+	{
+		--references;
+		if(!references) delete impl;
+	}
+}
---
+++ Graphics.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+namespace pwskoag
+{
+	class RendererImpl
+	{
+		friend class Renderer;
+		private:
+			sf::RenderWindow				window;
+			std::vector<sf::Drawable *> 	objectsToDraw;
+			void Draw()
+			{
+				window.Clear();
+				for(auto it=objectsToDraw.begin(); it!=objectsToDraw.end(); ++it) window.Draw(**it);
+				window.Display();
+			}
+			RendererImpl(uint w, uint h) : window(sf::VideoMode(w,h,32), "") {}
+	};
+
+	class Renderer
+	{
+		private:
+			static RendererImpl* 	impl;
+			static uint				references;
+		public:
+			Renderer(uint w, uint h);
+			~Renderer();
+			void AddObject(sf::Drawable& obj) {impl->objectsToDraw.push_back(&obj);}
+			void Draw() {impl->Draw();}
+	};
+	/*
+	class EventManager
+	{
+		private:
+			sf::Event e;
+		public:
+			sf::Event& getEvent() {Renderer::impl->window.PollEvent(e); return e;}
+	};
+	*/
+}
